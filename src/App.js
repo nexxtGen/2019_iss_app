2a1167c25b5452d034734410c20b0ae68add465c
Kamil Szczypior <kszczypior1@gmail.com>
2019-03-16 14:55:54 +0100

    create fetchData method and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    initialIssData: {
+      timestamp: "",
+      latitude: "",
+      longitude: ""
+    },
+    nextIssData: {
+      timestamp: "",
+      latitude: "",
+      longitude: "",
+
+    },
+    success: false,
+    errorMessage: ""
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.fetchDataFromApi();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  fetchDataFromApi = () => {
+    const url = `http://api.open-notify.org/iss-now.json`;
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        const issData = {
+          timestamp: data.timestamp,
+          latitude: data.iss_position.latitude,
+          longitude: data.iss_position.longitude
+        };
+        if (!this.state.initialIssData.timestamp) {
+          this.setState({
+            initialIssData: issData,            
+            errorMessage: ""
+          });
+        } else 
+          this.setState({
+            nextIssData: issData,
+            errorMessage: ""
+          });
+      })
+      .catch(error => {        
+        this.setState({ errorMessage: error.message });
+      });
+  }
+
   render() {
+    const {
+      initialIssData,
+      nextIssData,
+      errorMessage
+    } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>My app</h1>
+       <p>Initial Iss Data</p>
+       <p>Time: {initialIssData.timestamp}</p>
+       <p>Lat: {initialIssData.latitude}</p>
+       <p>Long: {initialIssData.longitude}</p>
+       <p>Current Iss data:</p>       
+       <p>Time: {nextIssData.timestamp}</p>
+       <p>Lat: {nextIssData.latitude}</p>
+       <p>Long: {nextIssData.longitude}</p>
+       <h4>Errors: {errorMessage}</h4>
       </div>
     );
   }
