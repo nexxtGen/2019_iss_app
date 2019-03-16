5bbabd5457f3010e00d58490f0cdeb5da2dfcaa7
Kamil Szczypior <kszczypior1@gmail.com>
2019-03-16 15:38:00 +0100

    add methods to calculate distance between points

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       longitude: "",
 
     },
+    distanceBetween: "",
     success: false,
     errorMessage: ""
   }
@@ -21,13 +22,20 @@ class App extends Component {
   componentDidMount() {
     this.interval = setInterval(() => {
       this.fetchDataFromApi();
+      if (this.state.nextIssData.timestamp) {
+        const {
+          initialIssData,
+          nextIssData
+        } = this.state;
+        this.getDistanceFromLatLonInKm(initialIssData.latitude, initialIssData.longitude, nextIssData.latitude, nextIssData.longitude, this.deg2rad);
+      }
     }, 1000);
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
-
+  //Get data from web API
   fetchDataFromApi = () => {
     const url = `http://api.open-notify.org/iss-now.json`;
     fetch(url)
@@ -54,15 +62,40 @@ class App extends Component {
       });
   }
 
+  //Calculate distance between two points using Haversine formula
+  getDistanceFromLatLonInKm = (lat1,lon1,lat2,lon2, deg2rad) => {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2-lat1);
+    const dLon = deg2rad(lon2-lon1); 
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = parseFloat((R * c).toFixed(1)); // Distance in km
+    this.setState({
+      distanceBetween: d
+    });
+  }
+
+  //Convert degrees to radians
+  deg2rad = (deg) => {
+    return deg * (Math.PI/180)
+  }
+
   render() {
     const {
       initialIssData,
       nextIssData,
-      errorMessage
+      errorMessage,
+      distanceBetween
     } = this.state;
+
     return (
       <div className="App">
        <h1>My app</h1>
+       <h5>Distance: {distanceBetween}km</h5>
        <p>Initial Iss Data</p>
        <p>Time: {initialIssData.timestamp}</p>
        <p>Lat: {initialIssData.latitude}</p>
